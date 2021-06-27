937ef66a327835f0f8beca8ac3d72da841532934
mutaqim96 <mutaqim.ah@theiac.com>
2021-06-28 04:39:27 +0800

    Create First React Component : test it

---
+++ music_controller/frontend/static/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { render } from "react-dom"
+
+
+// cara nak setup class dalam react
+export default class App extends{
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+
+    return <h1>Testing React Code</h1>
+  }
+}
