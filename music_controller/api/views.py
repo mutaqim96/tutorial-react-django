0e6a6e3af45ecc26522e3e8726e12f21ffc16b8a
mutaqim96 <mutaqim.ah@theiac.com>
2021-06-28 01:34:58 +0800

    Make API view, can return all room currently in db

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+#boleh buat class yang inherit daripada generics api views.
+from .serializers import RoomSerializer
+
 # Import httpresponse untuk bolehkan kita respond once receive request
-from django.http import  HttpResponse
+# from django.http import  HttpResponse
 
 # Create your views here.
 # write semua endpoint cth www.../apa2
 # location on webserver 
-def main(request):
-  return HttpResponse("<h1>Hello</h1>")
+# def main(request):
+#   return HttpResponse("<h1>Hello</h1>")
+
+class RoomView(generics.CreateAPIView):
+  queryset = Room.objects.all()
+  serializer_class = RoomSerializer
+
--- music_controller/notes.md
+++ music_controller/notes.md
@@ -1,3 +1,7 @@
 ## Notes
  * server automatic update kalau kita buat apa2 perubahan
  * request pergi ke main urls project dulu baru dia bawak ke urls dlm app
+ * backend - a server that can handle request and give some valid respond \
+ * Buat satu endpoint supaya frontend boleh tahu room mana yang created
+ * Mungkin backend tak hantar frontend HTML code. tapi akan hantar data
+ * Serializer , amek data dalam bentuk python daripada model dan translate jadi bentuk json
