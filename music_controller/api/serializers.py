e19d0b8d486cda05859a4cbf3e1ceb3a357ff033
mutaqim96 <mutaqim.ah@theiac.com>
2021-06-28 01:58:20 +0800

    Test the serializers

--- music_controller/api/serializers.py
+++ music_controller/api/serializers.py
@@ -4,6 +4,7 @@ from .models import Room
 class RoomSerializer(serializers.ModelSerializer):
   class Meta : 
     model = Room
-    # fields = ('id','code', 'host','guest_can_pause', 'vote_to_skip','created_at')
-    fields = '__all__'
+    fields = ('id','code', 'host','guest_can_pause', 'vote_to_skip','created_at')
+    # fields = '__all__'
+    # fields = ('id', 'code', 'host', 'guest_can_pause','vote_to_skip', 'created_at')
     
