9a8f1cea2e4073d8e1e15861c5fbb3703c554823
mutaqim96 <mutaqim.ah@theiac.com>
2021-06-28 01:19:39 +0800

    Fix : error : cannot run server + Make migrations

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -16,7 +16,7 @@ class Room(models.Model):
   code = models.CharField(max_length=8, default="", unique=True)
   host = models.CharField(max_length=50, unique=True)
   guest_can_pause = models.BooleanField(null=False, default =False)
-  vote_to_skip =models.IntegerField(_null=False,default=1)
+  vote_to_skip =models.IntegerField(null=False,default=1)
   created_at = models.DateTimeField(auto_now_add = True)
 
   # boleh add function jugak
